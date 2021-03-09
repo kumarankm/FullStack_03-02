d2f98e1079bd2cab501886bba1bdb36503f7ee7e
Kumaran KM <60292723+kumarankm@users.noreply.github.com>
2021-03-09 10:06:04 +0530

    Add files via upload

---
+++ serverRender.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+
+import App from './src/components/App';
+
+import config from './config';
+import axios from 'axios';
+
+const getApiUrl = contestId => {
+  if (contestId) {
+    return `${config.serverUrl}/api/contests/${contestId}`;
+  }
+  return `${config.serverUrl}/api/contests`;
+};
+
+const getInitialData = (contestId, apiData) => {
+  if (contestId) {
+    return {
+      currentContestId: apiData._id,
+      contests: {
+        [apiData._id]: apiData
+      }
+    };
+  }
+  return {
+    contests: apiData.contests
+  };
+};
+
+const serverRender = (contestId) =>
+  axios.get(getApiUrl(contestId))
+    .then(resp => {
+      const initialData = getInitialData(contestId, resp.data);
+      return {
+        initialMarkup: ReactDOMServer.renderToString(
+          <App initialData={initialData} />
+        ),
+        initialData
+      };
+    });
+
+export default serverRender;
